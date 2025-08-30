5d5a0e4cb3e882ed951040b97d2846576b057af6
vick61 <102034726+vick61@users.noreply.github.com>
2025-08-31 00:40:04 +0530

    Update supabase-kv_store.tsx

--- src/supabase/functions/server/kv_store.tsx
+++ src/supabase/functions/server/kv_store.tsx
@@ -10,7 +10,11 @@ CREATE TABLE kv_store_ce8ebc43 (
 // View at https://supabase.com/dashboard/project/baabzbunxucblvtojbfj/database/tables
 
 // This file provides a simple key-value interface for storing Figma Make data. It should be adequate for most small-scale use cases.
-import { createClient } from "jsr:@supabase/supabase-js@2.49.8";
+
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+
 
 const client = () => createClient(
   Deno.env.get("SUPABASE_URL"),
